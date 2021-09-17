24630c9476d5d6d6f1210d62d4c3194d1657da4e
Martijn <vandenberg@protonmail.ch>
2021-09-17 20:09:47 +0200

    added config file

---
+++ config.py
@@ -0,0 +1,11 @@
+# Operational config
+notifyWhenPriceLower = True
+notifyWhenPriceHigher = True
+notifyWhenPriceEqual = False
+
+# Secrets
+telegram_bot_enabled = False
+telegram_bot_token = ''
+telegram_bot_chat_id = ''
+
+
